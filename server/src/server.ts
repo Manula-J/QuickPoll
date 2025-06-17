79fa1bedd2c4f992ce0b722e3ac653c0d3bcc79f
Manula <manula.jayabodhi@gmail.com>
2025-06-17 20:00:43 +0530

    Changed database from Mongo DB to PostgreSQL

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,14 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import dotenv from "dotenv"
+import { Prisma, PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
 // express app
 const app = express();
+const prisma = new PrismaClient();
 
-// Connect to mongodb
-const dbURI: string = 'mongodb+srv://manulajayabodhi:wl0Ow9nBYeWF0TVf@clusterquickpoll.yonc6vi.mongodb.net/?retryWrites=true&w=majority&appName=ClusterQuickPoll';
-
-mongoose.connect(dbURI)
-  .then(() => {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-      console.log("Connected to MongoDB");
-    })
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1);
-  })
 
 app.use(express.json());
 
